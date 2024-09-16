0e8165939e72317f9fc2632f96ccfb1f251e6ccc
arefehmont <montazeri.arefeh@physics.sharif.edu>
2024-09-16 09:59:31 +0330

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -14,7 +14,7 @@
       { id: 3, title: '1984', author: 'George Orwell', readingStatus: 'read', img: '/images/book3.png' },
       { id: 4, title: 'The Lion, the Witch', author: 'C. S. Lewis', readingStatus: 'read', img: '/images/book4.png' },
       { id: 5, title: 'Kill Mockingbird', author: 'Harper Lee H.L  ', readingStatus: 'reading', img: '/images/book5.png' },
-      { id: 6, title: 'The Little Prince', author: 'Antoine du saunt Exupery', readingStatus: 'reading', img: '/images/book6.png' },
+      { id: 6, title: 'The Little Prince', author: 'Antoine Exupery', readingStatus: 'reading', img: '/images/book6.png' },
       { id: 7, title: 'Ghazaliyat', author: 'Saadi Shirazi', readingStatus: 'reading', img: '/images/book7.png' },
       { id: 8, title: 'Asrar Nameh', author: 'Attar Nishabouri', readingStatus: 'reading', img: '/images/book8.png' },
     
